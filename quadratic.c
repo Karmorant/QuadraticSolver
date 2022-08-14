c32331084d3c69299130155ba0898133a9a2f778
Karmorant <diz28600@gmail.com>
2022-08-14 19:42:57 +0300

    add irr

---
+++ quadratic.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+int main(){
+
+    double a, b, c, D;
+
+
+printf("Enter the first coefficient ");
+scanf("%lf", &a);
+printf("Enter the first coefficient ");
+scanf("%lf", &b);
+printf("Enter the first coefficient ");
+scanf("%lf", &c);
+
+if(c == 0){
+    printf("Equation roots:\n");
+    printf("0, %lf", (-b)/a);
+}
+else {
+    D = b*b - 4*a*c;
+    if(D < 0){
+        printf("Equation roots:\n");
+        printf("%lf + %lfi, %lf%lfi", (-b)/(2*a), (sqrt(D*(-1)))/(2*a), (-b)/(2*a), ((-1)*sqrt(D*(-1)))/(2*a));
+
+    }
+    else{
+        printf("Equation roots:\n");
+        printf("%lf, %lf", ((-b) + sqrt(D)) / (2*a), ((-b) - sqrt(D)) / (2*a));
+    }
+
+}
+
+    return  0;
+}
